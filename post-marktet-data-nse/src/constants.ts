48f9c64d4cb307ab81414a53e5035016191a7e0f
sarveshjanardhan <48390464+me027@users.noreply.github.com>
2024-07-01 22:04:38 +0530

    [test] github username issue check

--- post-marktet-data-nse/src/constants.ts
+++ post-marktet-data-nse/src/constants.ts
@@ -2,6 +2,7 @@ export const enum CONFIG {
 COOKIE_URL = 'https://www.nseindia.com/market-data/pre-open-market-cm-and-emerge-market',
 API_URL = 'https://www.nseindia.com/api/equity-stockIndices?csv=true&index=NIFTY%20500&selectValFormat=crores',
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+//somehow user-agent is needed for post market data and not pre market data. find this header as per your settings. 
 }
 
 export interface CookieInterface {
